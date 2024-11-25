c88c954dd396396f839949d3e1190e12b3d0263a
Alex Arasawa <aarasawa@gmail.com>
2024-11-24 23:26:47 -0800

    Trim files so portfolio renders

--- src/config.ts
+++ src/config.ts
@@ -40,6 +40,7 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Home,
     LinkPreset.Archive,
     LinkPreset.About,
+    LinkPreset.Portfolio,
     {
       name: 'GitHub',
       url: 'https://github.com/aarasawa/amber-echo',     // Internal links should not include the base path, as it is automatically added
